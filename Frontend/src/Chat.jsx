ec0d223dc13da1687193ac85f6ec621a9175c881
STIWARTs <stiwartsaxena477@gmail.com>
2025-09-12 11:51:00 +0530

    Format GPT Reply

--- Frontend/src/Chat.jsx
+++ Frontend/src/Chat.jsx
@@ -1,6 +1,10 @@
 import "./Chat.css";
 import {useContext} from "react";
 import { MyContext } from "./MyContext.jsx";
+import ReactMarkdown from "react-markdown";
+import rehypeHighlight from "rehype-highlight";
+import "highlight.js/styles/github-dark.css";
+
 
 function Chat() {
     const {newChat, prevChats} = useContext(MyContext);
@@ -11,12 +15,12 @@ function Chat() {
             <div className="chats">
                 {
                     prevChats?.map((chat, idx) => 
-                        <div key={idx} className={chat.role === "user" ? "userDiv" : "gptDiv"}>
-                        {
-                            chat.role === "user"?
-                            <p className="userMessage">{chat.content}</p>:
-                            <p className="gptMessage">{chat.content}</p>
-                        }
+                        <div className={chat.role === "user"? "userDiv" : "gptDiv"} key={idx}>
+                            {
+                                chat.role === "user"? 
+                                <p className="userMessage">{chat.content}</p> : 
+                                <ReactMarkdown rehypePlugins={[rehypeHighlight]}>{chat.content}</ReactMarkdown>
+                            }
                         </div>
                     )
                 }
