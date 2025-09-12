e8b985b3d72802a1b820f134408c8336c9603b7e
STIWARTs <stiwartsaxena477@gmail.com>
2025-09-12 11:35:46 +0530

    Display Chats

--- Frontend/src/Chat.jsx
+++ Frontend/src/Chat.jsx
@@ -3,19 +3,32 @@ import {useContext} from "react";
 import { MyContext } from "./MyContext.jsx";
 
 function Chat() {
-    const {newChat, prevChats, reply} = useContext(MyContext);
+    const {newChat, prevChats} = useContext(MyContext);
 
     return (
         <>
             {newChat && <h1> Start a New Chat!</h1>}
             <div className="chats">
-                <div className="userDiv">
+                {
+                    prevChats?.map((chat, idx) => 
+                        <div key={idx} className={chat.role === "user" ? "userDiv" : "gptDiv"}>
+                        {
+                            chat.role === "user"?
+                            <p className="userMessage">{chat.content}</p>:
+                            <p className="gptMessage">{chat.content}</p>
+                        }
+                        </div>
+                    )
+                }
+            </div>
+            {/* STATIC DATA */}
+                {/* <div className="userDiv">
                     <p className="userMessage"> User Message </p>
                 </div>
                 <div className="gptDiv">
                     <p className="gptMessage">GPT Generated Message</p>
-            </div>
-            </div>
+            </div> */}
+            
         </>
     )
 }
--- Frontend/src/ChatWindow.jsx
+++ Frontend/src/ChatWindow.jsx
@@ -1,12 +1,12 @@
 import "./ChatWindow.css";
 import Chat from "./Chat.jsx";
 import { MyContext } from "./MyContext.jsx";
-import { useContext, useState} from "react";
+import { useContext, useState, useEffect} from "react";
 import {ScaleLoader} from "react-spinners";
 
 
 function ChatWindow() {
-    const {prompt, setPrompt, reply, setReply, currThreadId} = useContext(MyContext);
+    const {prompt, setPrompt, reply, setReply, currThreadId, prevChats, setPrevChats} = useContext(MyContext);
     const [loading, setLoading] = useState(false);
 
     //creating getReply function to call backend api
@@ -33,6 +33,23 @@ function ChatWindow() {
         }
         setLoading(false);     
     }
+
+     //Append new chat to prevChats
+    useEffect(() => {
+        if(prompt && reply) { //when both prompt and reply are present/exist
+            setPrevChats(prevChats => ( //assigning value to prev chat
+                [...prevChats, {
+                    role: "user",
+                    content: prompt
+                },{
+                    role: "assistant",
+                    content: reply
+                }]
+            ));
+        }
+
+        setPrompt("");
+    }, [reply]);
 
     return (
         <div className="chatWindow">
